7a3069c5ec3ac0d6235a7fb5133086638ba67dc1
tamal <tamal@appscode.com>
2017-03-08 18:01:03 -0800

    Add Dockerfiles and deployment yaml for scheduler.

---
+++ plugin/deploy/kube-scheduler/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+RETVAL=0
+
+cmd=build
+if [ $# -ne 0 ]; then
+    cmd="$1"
+fi
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT=$GOPATH/src/k8s.io/kubernetes
+IMG=hostpath-scheduler
+
+build() {
+    pushd $REPO_ROOT/plugin/deploy/kube-scheduler
+    tag=$(git describe --tags --always --dirty)
+    cp $REPO_ROOT/_output/local/bin/linux/amd64/kube-scheduler .
+    chmod +x kube-scheduler
+    docker build -t appscode/$IMG:$tag .
+    rm kube-scheduler
+    docker push appscode/$IMG:$tag
+    popd
+}
+
+case "$cmd" in
+    build)
+        build
+        ;;
+    *)	(10)
+        echo $"Usage: $0 {build}"
+        RETVAL=1
+esac
+exit $RETVAL
